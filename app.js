ad9975c945864d55996da992adbf040a1057cc54
zhongfc <zhongfengcheng@xyac.com.cn>
2020-10-24 23:01:54 +0800

    end to end test

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
-const express = require("express");
-const cors = require("cors");
-require("express-async-errors");
-const app = express();
-const blogsRouter = require("./controllers/blogs");
-const usersRouter = require("./controllers/users");
-const loginRouter = require("./controllers/login");
-const config = require("./utils/config");
-const mongoose = require("mongoose");
-const middleware = require("./utils/middleware");
+const express = require('express')
+const cors = require('cors')
+require('express-async-errors')
+const app = express()
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 mongoose
   .connect(config.MONGODB_URI, {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
   .then(() => {
-    console.log("connected:", config.MONGODB_URI);
+    console.log('connected:', config.MONGODB_URI)
   })
   .catch((error) => {
-    console.log("connect error:", error.message);
-  });
+    console.log('connect error:', error.message)
+  })
 
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
-app.use("/api/login", loginRouter);
-app.use("/api/blogs", blogsRouter);
-app.use("/api/users", usersRouter);
+app.use('/api/login', loginRouter)
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
-app.use(middleware.unkonwnEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unkonwnEndpoint)
+app.use(middleware.errorHandler)
 
-module.exports = app;
+module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,71 +1,72 @@
-const blogRouter = require("express").Router();
-const jwt = require("jsonwebtoken");
-const Blog = require("../models/blog");
-const User = require("../models/user");
+const blogRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
-  console.log(blogs);
-  response.json(blogs);
-});
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  console.log(blogs)
+  response.json(blogs)
+})
 
-blogRouter.post("/", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+blogRouter.post('/', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+    return response.status(401).json({ error: 'token missing or invalid' })
   }
 
-  const body = request.body;
-  const user = await User.findById(decodedToken.id);
+  const body = request.body
+  const user = await User.findById(decodedToken.id)
+  console.log(user)
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: user._id,
-  });
+    user: user._id
+  })
 
   if (!blog.title || !blog.url) {
-    response.status(400).end();
+    response.status(400).end()
   } else {
-    const savedBlog = await blog.save();
-    user.blogs = user.blogs.concat(savedBlog);
-    await user.save();
-    response.json(savedBlog);
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog)
+    await user.save()
+    response.json(savedBlog)
   }
-});
+})
 
-blogRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+blogRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!request.token || !decodedToken) {
-    response.status(401).end({ error: "Unauthorized" });
+    response.status(401).end({ error: 'Unauthorized' })
   }
-  const blog = await Blog.findById(request.params.id);
+  const blog = await Blog.findById(request.params.id)
   if (blog) {
     if (blog.user.toString() == decodedToken.id) {
-      await blog.remove();
+      await blog.remove()
       response.status(204).end()
-    }else{
-      response.status(401).json({error:"no permission"});
+    } else {
+      response.status(401).json({ error: 'no permission' })
     }
   } else {
-    response.status(400).json({ error: "the blog is not exist" });
+    response.status(400).json({ error: 'the blog is not exist' })
   }
-});
+})
 
-blogRouter.put("/:id", async (request, response) => {
+blogRouter.put('/:id', async (request, response) => {
   const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
     request.body,
     {
-      new: true,
+      new: true
     }
-  );
+  )
   if (updatedBlog) {
-    response.json(updatedBlog);
+    response.json(updatedBlog)
   } else {
-    response.status(400).end();
+    response.status(400).end()
   }
-});
+})
 
-module.exports = blogRouter;
+module.exports = blogRouter
